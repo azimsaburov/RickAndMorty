62a3d9ccc3d548da7afbce2758e44afab674c9f9
Azim <azimsab2000@gmail.com>
2025-06-26 16:05:19 +0600

    init

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:rick_and_morty/screen/home_screen.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: HomeScreen());
+  }
+}
