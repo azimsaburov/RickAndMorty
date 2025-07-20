ddbfd884965d852fd91ec60043e4a61e23917ad8
Azim <azimsab2000@gmail.com>
2025-07-20 21:09:01 +0600

    Пример сотировки элементов в списке

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,17 @@ import 'package:rick_and_morty/presentation/characters/ui/bloc/character_bloc/ch
 import 'package:rick_and_morty/home_screen.dart';
 import 'package:rick_and_morty/theme_constants.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  //Пример сортировки элементов в списке
+  // List<String> textSort = ['s', 'f', 'a', 'c', 'b', 'abc'];
+  // List<int> numSort = [2, 4, 233, 1, 11, 49];
+  // textSort.sort((a, b) => a.compareTo(b));
+  // numSort.sort();
+  // print(textSort);
+  // print(numSort);
+
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -13,10 +23,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [BlocProvider(create: (context) => CharacterBloc())],
-      child: MaterialApp(
-        home: HomeScreen(),
-        theme: ThemeConstants.lightTheme,
-      ),
+      child: MaterialApp(home: HomeScreen(), theme: ThemeConstants.lightTheme),
     );
   }
 }
